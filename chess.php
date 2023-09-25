d190c2757ad5aaa7d2fe24773d0a7524c2033da7
Tobibit <98736566+Tobibit@users.noreply.github.com>
2023-09-25 15:31:44 +0200

    Starting files

---
+++ chess.php
@@ -0,0 +1,14 @@
+<?php
+    // draw the chess board
+    for ($row = 1; $row <= 8; $row++) {
+        for ($col = 1; $col <= 8; $col++) {
+            $total = $row + $col;
+            if ($total % 2 == 0) {
+                echo "<div class='black'></div>";
+            } else {
+                echo "<div class='white'></div>";
+            }
+        }
+        echo "<br>";
+    }
+?>
