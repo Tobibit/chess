b0fcd346b27b79729c34253e21d8deead44b06c0
Tobibit <tobias.viebahn@rosenberger.com>
2023-09-26 07:25:43 +0200

    draw board

--- chess.php
+++ chess.php
@@ -1,16 +1,32 @@
-<?php
-    // include style
-    echo "<link rel='stylesheet' type='text/css' href='style.css' />";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' href='style.css' />
+    <title>Document</title>
+</head>
+<body>
+    <table class="boardTable">
+        <?php
+            $value = 0;
 
-    // draw the chess board
-    for ($row = 1; $row <= 8; $row++) {
-        for ($col = 1; $col <= 8; $col++) {
-            $total = $row + $col;
-            if ($total % 2 == 0) {
-                echo "<div class='black'></div>";
-            } else {
-                echo "<div class='white'></div>";
+            for($col = 0; $col < 8; $col++){
+                echo "<tr>";
+                $value = $col;
+
+                for($row = 0; $row < 8; $row++){
+                    if($value % 2 == 0){
+                        echo "<td class='black'></td>";
+                        $value++;
+                    } else {
+                        echo "<td class='white'></td>";
+                        $value++;
+                    }
+                }
+                echo "</tr>";
             }
-        }
-    }
-?>
+        ?>
+    </table>
+</body>
+</html>
