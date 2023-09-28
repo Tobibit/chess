2b38187f7ecbc5777c8433b30fc19142c21fd877
Tobibit <tobiasviebahn2005@gmail.com>
2023-09-28 11:22:31 +0200

    class for each piece that tracks position and color

---
+++ pieces/Queen.php
@@ -0,0 +1,33 @@
+<?php
+    require_once("./movements.php");
+    class Queen {
+        private $x;
+        private $y;
+        private $color;
+
+        public function __construct($x, $y, $color) {
+            $this->x = $x;
+            $this->y = $y;
+            $this->color = $color;
+        }
+
+        public function getX() {
+            return $this->x;
+        }
+
+        public function getY() {
+            return $this->y;
+        }
+
+        public function getColor() {
+            return $this->color;
+        }
+
+        public function move($newX, $newY) {
+            if(isValid("queen", $this->x, $this->y, $newX, $newY)){
+                $this->x = $newX;
+                $this->y = $newY;
+            }
+        }
+    }
+?>
