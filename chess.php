dca7c086427e1ad4e6c9006dd9aeb96b12dbaa67
Tobibit <tobiasviebahn2005@gmail.com>
2023-09-28 14:34:02 +0200

    notationTranslation: more accurate file name

--- chess.php
+++ chess.php
@@ -10,7 +10,7 @@
     <table class="boardTable">
         <?php
         include_once("./classFunctions.php");
-        include_once("./notation.php");
+        include_once("./notationTranslation.php");
 
         $board = [];
 
