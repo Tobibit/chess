a6f87f5b2a547a3de7c25601d99f8a800daa5032
Tobibit <tobiasviebahn2005@gmail.com>
2023-09-30 22:08:54 +0200

    modified startPosition.php

--- startPosition.php
+++ startPosition.php
@@ -2,45 +2,23 @@
     include_once "./includes.php";
 
     function startPosition() {
-        $pieces = array([
-            // white  
-            "white_Rook1" => new Rook ("white", "rook1"),
-            "white_Knight1" => new Knight ("white", "knight1"),
-            "white_Bishop1" => new Bishop ("white", "bishop1"),
-            "white_Queen" => new Queen ("white", "queen"),
-            "white_King" => new King ("white", "king"),
-            "white_Bishop2" => new Bishop ("white", "bishop2"),
-            "white_Knight" => new Knight ("white", "knight2"),
-            "white_Rook2" => new Rook ("white", "rook2"),
-            "white_Pawn1" => new Pawn ("white", "pawn1"),
-            "white_Pawn2" => new Pawn ("white", "pawn2"),
-            "white_Pawn3" => new Pawn ("white", "pawn3"),
-            "white_Pawn4" => new Pawn ("white", "pawn4"),
-            "white_Pawn5" => new Pawn ("white", "pawn5"),
-            "white_Pawn6" => new Pawn ("white", "pawn6"),
-            "white_Pawn7" => new Pawn ("white", "pawn7"),
-            "white_Pawn8" => new Pawn ("white", "pawn8")
-            ], 
-            // black
-            [
-            "black_Rook1" => new Rook ("black", "rook1"),
-            "black_Knight1" => new Knight ("black", "knight1"),
-            "black_Bishop1" => new Bishop ("black", "bishop1"),
-            "black_Queen" => new Queen ("black", "queen"),
-            "black_King" => new King ("black", "king"),
-            "black_Bishop2" => new Bishop ("black", "bishop2"),
-            "black_Knight" => new Knight ("black", "knight2"),
-            "black_Rook2" => new Rook ("black", "rook2"),
-            "black_Pawn1" => new Pawn ("black", "pawn1"),
-            "black_Pawn2" => new Pawn ("black", "pawn2"),
-            "black_Pawn3" => new Pawn ("black", "pawn3"),
-            "black_Pawn4" => new Pawn ("black", "pawn4"),
-            "black_Pawn5" => new Pawn ("black", "pawn5"),
-            "black_Pawn6" => new Pawn ("black", "pawn6"),
-            "black_Pawn7" => new Pawn ("black", "pawn7"),
-            "black_Pawn8" => new Pawn ("black", "pawn8")
-            ]);
-
+        $pieces = [];
+    
+        // Create pieces for white and black
+        $colors = ["white", "black"];
+        $pieceNames = ["Rook1", "Knight1", "Bishop1", "Queen", "King", "Bishop2", "Knight2", "Rook2"];
+        $pawnNames = ["Pawn1", "Pawn2", "Pawn3", "Pawn4", "Pawn5", "Pawn6", "Pawn7", "Pawn8"];
+    
+        foreach ($colors as $color) {
+            foreach ($pieceNames as $name) {
+                $pieces[$color . "_" . $name] = new $name($color, strtolower($name));
+            }
+    
+            foreach ($pawnNames as $name) {
+                $pieces[$color . "_" . $name] = new Pawn($color, strtolower($name));
+            }
+        }
+    
         return $pieces;
-    }
+    }    
 ?>
