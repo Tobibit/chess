81a9d32eb4abcabd1e47ca17e9c3ce47c264f58c
Tobibit <tobiasviebahn2005@gmail.com>
2023-10-10 13:31:49 +0200

    piece themes

--- game.js
+++ game.js
@@ -1,6 +1,29 @@
-var config = {
-    position: 'start',
-    draggable: true
+function pieceTheme(piece, input) {
+    // choose theme based on user input (dropdown)
+    input = "derangedHorsey";
+    switch(input) {
+        case 'derangedHorsey': 
+            return './assets/pieceThemes/derangedHorsey/' + piece + '.png'; 
+        // add cases
+        default: return './assets/pieceThemes/standard/' + piece + '.png';
+    }
 }
 
-var board1 = ChessBoard('board1', config);
+var configCustom = {
+    position: 'empty',
+    draggable: true,
+    showNotation: true,
+    dropOffBoard: 'trash',
+    pieceTheme: pieceTheme,
+}
+
+var configGame = {
+    position: 'start',
+    draggable: true,
+    showNotation: true,
+    dropOffBoard: 'snapback',
+    pieceTheme: pieceTheme,
+}
+
+var board1 = ChessBoard('board1', configGame);
+var boardCustom = ChessBoard('boardCustom', configCustom);
