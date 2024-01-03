0811f222789384233cd3e22b13048bc93efdb7cc
Tobibit <tobiasviebahn2005@gmail.com>
2024-01-03 10:07:45 +0100

    initialize standard chessjs functions

--- src/playEngine.js
+++ src/playEngine.js
@@ -1,5 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
-const engineStrength = urlParams.get('engineStrength');
+const engineStrength = urlParams.get('engineStrength'); // depth
 const orientation = urlParams.get('orientation');
 const notation = urlParams.get('notation');
 
@@ -18,6 +18,8 @@ function pieceTheme(piece) {
     }
 }
 
+// var game = new Chess();
+
 var config = {
     position: 'start',
     draggable: true,
@@ -25,6 +27,64 @@ var config = {
     dropOffBoard: 'snapback',
     pieceTheme: pieceTheme,
     orientation: orientation,
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onDragEnd: onDragEnd,
 }
 
 var board = ChessBoard('board', config);
+
+function onDragStart (source, piece, position, orientation) {
+    return game.game_over() === false
+}
+
+function onDrop (source, target) {
+    // see if the move is legal
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    })
+
+    // illegal move
+    if (move === null) return 'snapback'
+
+    updateStatus()
+    makeEngineMove()
+}
+
+var onDragEnd = function() {
+    board.position(game.fen())
+}
+
+function makeEngineMove() {
+    var depth = engineStrength;
+    var move = getBestMove(depth);
+    game.move(move);
+    board.position(game.fen());
+    updateStatus();
+}
+
+function updateStatus() {
+    var status = '';
+
+    var moveColor = 'White';
+    if (game.turn() === 'b') {
+        moveColor = 'Black';
+    }
+
+    if (game.in_checkmate()) {
+        status = 'Game over, ' + moveColor + ' is in checkmate.';
+    } else if (game.in_draw()) {
+        status = 'Drawn position';
+    } else if (game.in_stalemate()) {
+        status = 'Draw! ' + moveColor + ' is in stalemate.';
+    } else {
+        status = moveColor + ' to move';
+        if (game.in_check()) {
+            status += ', ' + moveColor + ' is in check';
+        }
+    }
+
+    $('#status').html(status);
+}
